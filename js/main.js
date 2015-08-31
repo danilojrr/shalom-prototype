e2066a418f314b90949665768f1be262bc07d051
Danilo Barros <danilobjr@gmail.com>
2015-08-31 17:29:02 -0300

    check member presence

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(function () {
 	'use strict';
 
+    // dropdown menu on mouse enter
+
 	$('.navbar .dropdown').on('mouseenter', openDropdown);
 	$('.navbar .dropdown').on('mouseleave', closeDropdown);
 
@@ -15,4 +17,16 @@ $(function () {
 	function closeDropdown(e) {
 		$(e.currentTarget).removeClass('open');
 	}
+
+
+    // check member presence
+
+    $('.check-presence').on('click', togglePresence);
+
+    function togglePresence(e) {
+        var _this = $(e.currentTarget);
+
+        _this.find('.present').toggle();
+        _this.find('.not-present').toggle();
+    }
 });
